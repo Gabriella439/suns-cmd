2897e7cb7f0f379e39e00a03185b4b43134d81dc
Gabriel Gonzalez <Gabriel439@gmail.com>
2013-09-06 12:46:12 -0700

    Factored the search out into a `Producer`
    This is so that the test suite can reuse it without having to shell out to call
    the executable

--- suns-cmd.hs
+++ suns-cmd.hs
@@ -1,8 +1,7 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, TypeFamilies #-}
 
-import Control.Concurrent.MVar (MVar, newEmptyMVar, putMVar, takeMVar)
-import Control.Error (runScript, (??), scriptIO, errLn)
-import Control.Exception (bracket)
+import Control.Error (errLn)
+import Control.Monad (void)
 import Data.Aeson ((.=), object, encode)
 import qualified Data.ByteString.Char8 as B
 import qualified Data.ByteString.Lazy as BL
@@ -10,14 +9,18 @@ import Data.IORef (IORef, newIORef, readIORef, modifyIORef)
 import qualified Data.Map as M
 import Data.Monoid (mconcat)
 import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
 import Data.UUID.V4 (nextRandom)
+import qualified Filesystem.Path.CurrentOS as F
+import Filesystem.Path ((</>), (<.>))
+import qualified Filesystem as F
 import Network (withSocketsDo)
 import Network.AMQP
 import Network.AMQP.Types (FieldTable(FieldTable))
 import Options.Applicative
-import qualified Filesystem.Path.CurrentOS as F
-import Filesystem.Path ((</>), (<.>))
-import qualified Filesystem as F
+import Pipes
+import Pipes.Safe
+import Pipes.Concurrent
 
 data Options = Options
     { _hostName  :: String
@@ -87,43 +90,64 @@ responseExchange = "suns-exchange-responses"
 main :: IO ()
 main = withSocketsDo $ do
     Options hostName rmsd numStruct seed directory <- execParser parserInfo
+    query <- TIO.getContents
+    runSafeT $ runEffect $ do
+        let results = search hostName rmsd numStruct seed query
+        for results $ \(pdbID, n, pdbStr) -> liftIO $ do
+            let fileName =
+                        directory
+                    </> F.decodeString (T.unpack pdbID ++ "_" ++ show n)
+                    <.> "pdb"
+            F.writeTextFile fileName pdbStr
+
+search
+    :: (MonadSafe m, Base m ~ IO)
+    => String
+    -> Double
+    -> Int
+    -> Int
+    -> T.Text
+    -> Producer' (T.Text, Integer, T.Text) m ()
+search hostName rmsd numStruct seed pdb = do
+    (output, input, doneOutput, _) <- liftIO $ spawn' Unbounded
     bracket
         (openConnection hostName "suns-vhost" "suns-client" "suns-client")
         closeConnection
-        $ \connection -> runScript $ do
-            channel <- scriptIO $ openChannel connection
-            scriptIO $ declareExchange channel $ ExchangeOpts
+        $ \connection -> do
+            channel <- liftIO $ openChannel connection
+            liftIO $ declareExchange channel $ ExchangeOpts
                 responseExchange  -- exchangeName
                 "direct"          -- exchangeType
                 True              -- exchangePassive
                 True              -- exchangeDurable
                 False             -- exchangeAutoDelete
                 False             -- exchangeInternal
-            scriptIO $ declareExchange channel $ ExchangeOpts
+            liftIO $ declareExchange channel $ ExchangeOpts
                 requestExchange  -- exchangeName
                 "direct"         -- exchangeType
                 True             -- exchangePassive
                 True             -- exchangeDurable
                 False            -- exchangeAutoDelete
                 False            -- exchangeInternal
-            (qName, _, _) <- scriptIO $ declareQueue channel $ QueueOpts
+            (qName, _, _) <- liftIO $ declareQueue channel $ QueueOpts
                 ""                    -- queueName
                 False                 -- queuePassive
                 False                 -- queueDurable
                 True                  -- queueExclusive
                 True                  -- queueAutoDelete
                 (FieldTable M.empty)  -- queueHeaders
-            scriptIO $ bindQueue channel qName responseExchange qName
-            pdb  <- scriptIO B.getContents
-            uID  <- scriptIO nextRandom
-            mvar <- scriptIO newEmptyMVar
-            used <- scriptIO $ newIORef M.empty
+            liftIO $ bindQueue channel qName responseExchange qName
+            uID  <- liftIO nextRandom
+            used <- liftIO $ newIORef M.empty
             let uIDtxt = T.pack (show uID)
-            scriptIO $ bracket
+            bracket
                 (consumeMsgs
-                     channel qName Ack (callback uIDtxt mvar used directory) )
+                     channel
+                     qName
+                     Ack
+                     (callback uIDtxt used output (atomically doneOutput)) )
                 (cancelConsumer channel)
-                $ \_ -> runScript $ do
+                $ \_ -> do
                     let msg = newMsg
                             { msgBody = encode $ object
                                 [ "rmsd_cutoff"    .= rmsd
@@ -134,44 +158,49 @@ main = withSocketsDo $ do
                             , msgReplyTo = Just qName
                             , msgCorrelationID = Just uIDtxt
                             }
-                    scriptIO $ publishMsg channel requestExchange "1.0.0" msg
-                    scriptIO $ takeMVar mvar
+                    liftIO $ publishMsg channel requestExchange "1.0.0" msg
+                    fromInput input
+--                  takeMVar mvar
 
 callback
     :: T.Text
-    -> MVar ()
-    -> IORef (M.Map B.ByteString Int)
-    -> F.FilePath
+    -> IORef (M.Map B.ByteString Integer)
+    -> Output (T.Text, Integer, T.Text)
+    -> IO ()
     -> (Message, Envelope)
     -> IO ()
-callback uIDtxt mvar used directory (message, _envelope) = runScript $
+callback uIDtxt used output done (message, _envelope) =
     case (msgCorrelationID message) of
         Nothing                        -> return ()
         Just corrID | corrID /= uIDtxt -> return ()
                     | otherwise -> do
             let body = B.concat (BL.toChunks (msgBody message))
-            (tag, rest) <- B.uncons body ?? emptyErr
-            case tag of
-                '0' -> scriptIO $ putMVar mvar ()
-                '1' -> do
-                    let (pdbID, match) = B.splitAt 4 rest
-                    m <- scriptIO $ readIORef used
-                    let n = maybe 0 id (M.lookup pdbID m)
-                    scriptIO $ modifyIORef used (M.insertWith (+) pdbID 1)
-                    let fileName =
-                                directory
-                            </> F.decodeString (B.unpack pdbID ++ "_" ++ show n)
-                            <.> "pdb"
-                    scriptIO $ F.writeFile fileName match
-                '2' -> scriptIO $ do
-                     errLn "Server time limit exceeded"
-                     putMVar mvar ()
-                '3' -> scriptIO $ do
-                     errLn $ "Server Error: " ++ B.unpack rest
-                     putMVar mvar ()
-                _   -> scriptIO $ do
-                     errLn $ invalidErr body
-                     putMVar mvar ()
+            case (B.uncons body) of
+                Nothing -> do
+                     errLn emptyErr
+                     done
+                Just (tag, rest) -> do
+                    case tag of
+                        '0' -> done
+                        '1' -> do
+                            let (pdbID, match) = B.splitAt 4 rest
+                            m <- readIORef used
+                            let n = maybe 0 id (M.lookup pdbID m)
+                            modifyIORef used (M.insertWith (+) pdbID 1)
+                            void $ atomically $ send output
+                                ( T.pack (B.unpack pdbID)
+                                , n
+                                , T.pack (B.unpack match)
+                                )
+                        '2' -> do
+                             errLn "Server time limit exceeded"
+                             done
+                        '3' -> do
+                             errLn $ "Server Error: " ++ B.unpack rest
+                             done
+                        _   -> do
+                             errLn $ invalidErr body
+                             done
 
 emptyErr :: String
 emptyErr = "\
